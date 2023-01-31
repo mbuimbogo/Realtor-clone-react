7e433a1984a3908cde049ca65153ba19e3af0ace
mbuimbogo <mbuimbogo99@gmail.com>
2023-02-01 01:44:34 +0300

    create sign in, sign up and forgotb passwords and oauth component

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Home from './pages/Home'
 import Profile from './pages/Profile'
 import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
-import ForgotPassword from './pages/Home'
+import ForgotPassword from './pages/ForgotPassword'
 import Offers from './pages/Offers'
 import Header from './components/Header'
 const App = () => {
