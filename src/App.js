599fae2c8fb93d79b26dd1d2835f3412929e603d
mbuimbogo <mbuimbogo99@gmail.com>
2023-01-31 13:28:03 +0300

    create header component

--- src/App.js
+++ src/App.js
@@ -6,10 +6,12 @@ import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
 import ForgotPassword from './pages/Home'
 import Offers from './pages/Offers'
+import Header from './components/Header'
 const App = () => {
   return (
     <>
     <Router>
+        <Header/>
         <Routes>
             <Route path = "/" element={<Home/>}/>
             <Route path = "/profile" element={<Profile/>}/>
