a45d8dc25fe5cb34afccc5f994fc6f54dc49ed46
mbuimbogo <mbuimbogo99@gmail.com>
2023-02-14 12:05:02 +0300

    fix: add functionality for the input fields

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -25,8 +25,28 @@ const CreateListing = () => {
     
 
     
-function onChange(){
+function onChange(e){
+let boolean = null;
+if(e.target.value === "true"){
+    boolean = true
+}
 
+if(e.target.value === "false"){
+    boolean = false
+}
+
+// files
+if(e.target.files){
+    setFormData((prevState)=>({
+        ...prevState, images:e.target.files
+    }))
+}
+//text/boolean/number
+if(!e,target.files){
+    setFormData((prevState) =({
+        ...prevState, [e.target.id]: boolean ?? e.target.value
+    }))
+}
 
 }
 
@@ -39,7 +59,7 @@ function onChange(){
             </p>
             <div className='flex'>
                 <button type='button' id='type' value="sale" onClick={onChange} className={` mr-3px-7 py-3 font-medium text-sm uppercase shadow-md rounded hover:shadow-lg focus:shadow-lg active:shadow-lg transition duration-150 ease-in-out w-full ${ type === "rent" ? "bg-white text-black" : "bg-slate-600 text-white"}`}>Sell</button>
-                <button type='button' id='type' value="sale" onClick={onChange} className={`ml-3px-7 py-3 font-medium text-sm uppercase shadow-md rounded hover:shadow-lg focus:shadow-lg active:shadow-lg transition duration-150 ease-in-out w-full ${ type === "sale" ? "bg-white text-black" : "bg-slate-600 text-white"}`}>Rent</button>
+                <button type='button' id='type' value="rent" onClick={onChange} className={`ml-3px-7 py-3 font-medium text-sm uppercase shadow-md rounded hover:shadow-lg focus:shadow-lg active:shadow-lg transition duration-150 ease-in-out w-full ${ type === "sale" ? "bg-white text-black" : "bg-slate-600 text-white"}`}>Rent</button>
              </div>
              <p className='text-lg mt-6 font-semibold'>Name</p>
              <input type="text" id='name' value={name} onChange={onChange} placeholder="Name" maxLength="32" minLength="10" required className='w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition duration-150 ease-in-out focus:text-gray-700 focus:bg-white focus:border-slate-600 mb-6'
