2c87e768497dcfbfb26730d2bdfd0aad6fb80a14
mbuimbogo <mbuimbogo99@gmail.com>
2023-02-14 11:39:48 +0300

    feat: create a listing button on profile page

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,9 +1,10 @@
 import React, { useState } from 'react'
 import { getAuth, updateProfile } from "firebase/auth";
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
 import { db } from '../firebase';
 import { doc, updateDoc } from 'firebase/firestore';
+import { FcHome } from "react-icons/fc"
 
 export default function Profile() {
 
@@ -73,6 +74,14 @@ export default function Profile() {
               <p onClick={onLogOut} className='text-blue-600 hover:text-blue-800 transition duration-200 ease-in-out cursor-pointer'>Sign Out</p>
             </div>
           </form>
+
+          <button type='submit' className='w-full bg-blue-600 text-white uppercase px-7 py-3 text-sm font-medium rounded shadow-md hover:bg-blue-700 transition duratio-150 ease-in-out hover:shadow-lg active:bg-blue-800'>
+            <Link to="/create-listing" className='flex justify-center items-center'>
+            <FcHome className='mr-2 text-3xl bg-red-200 rounded-full p-1 border-2'/>
+            Sell or rent your home
+            </Link>
+           
+          </button>
         </div>
         </section>  
     </>
