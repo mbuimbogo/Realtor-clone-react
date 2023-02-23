2a08fc569ee5221032264ec6372813a6e873087a
mbuimbogo <mbuimbogo99@gmail.com>
2023-02-23 21:53:22 +0300

    create EditListing page

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import Header from './components/Header'
 import { ToastContainer } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
 import CreateListing from './pages/CreateListing'
+import EditListing from './pages/EditListing'
 const App = () => {
   return (
     <>
@@ -27,6 +28,9 @@ const App = () => {
             <Route path = "/offers" element={<Offers/>}/>
             <Route path='create-listing' element={<PrivateRoute/>}>
             <Route path = "/create-listing" element={<CreateListing/>}/>
+            </Route>
+            <Route path='edit-listing' element={<PrivateRoute/>}>
+            <Route path = "/edit-listing/:listingId" element= {<EditListing/>}/>
             </Route>
         </Routes>
     </Router>
