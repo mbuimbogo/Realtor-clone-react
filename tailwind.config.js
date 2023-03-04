3401d84c48a5c021087a7c83a1a993cee5576842
mbuimbogo <mbuimbogo99@gmail.com>
2023-03-05 01:03:09 +0300

    edit tailwind configaration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,8 @@
-/* @type {import('tailwindcss').Config} */
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
   },
-  plugins: [ require('@tailwindcss/forms')],
+  plugins: [],
 }
